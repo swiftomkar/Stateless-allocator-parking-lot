a33c311478ed91825255836a2f377b35ba5d0ede
omkardesai <omkar.desai@practo.com>
2019-07-13 13:55:28 +0530

    interface for data structure functions

---
+++ src/main/java/com/assignment/parkinglot/DataStructure/AbstractParkingDataStructure.java
@@ -0,0 +1,24 @@
+package com.assignment.parkinglot.DataStructure;
+
+import com.assignment.parkinglot.objectmodels.Vehicle;
+
+import java.util.List;
+
+public interface AbstractParkingDataStructure<T extends Vehicle> {
+
+  public int parkCar(T vehicle);
+
+  public boolean leave(int slot);
+
+  public List<String> getStatus();
+
+  public List<String> getRegNumberForColor(String color);
+
+  public List<Integer> getSlotNumbersFromColor(String colour);
+
+  public int getSlotNoFromRegistrationNo(String registrationNo);
+
+  public int getNumFreeSlots();
+
+
+}
--- src/main/java/com/assignment/parkinglot/parkinglotservice/ParkingLotServiceImpl.java
+++ src/main/java/com/assignment/parkinglot/parkinglotservice/ParkingLotServiceImpl.java
@@ -38,7 +38,4 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     return data==null;
   }
 
-
-
-
 }
